46161af368a40188ed1ecb5eba71985efd0f7531
charliejuc <charliejuc@gmail.com>
2020-05-28 13:09:17 +0200

    Decorator Pattern Mockup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 import 'module-alias/register'
-import { hola } from '@/other'
+import 'source-map-support/register'
+import { CreateRepository } from './implementations/CreateRepository'
+import { TransactionDecorator } from './implementations/decorators/TransactionDecorator'
+import { CreateObject } from './interfaces/CreateObject'
+import { ICreateRepository } from './interfaces/ICreateRepository'
 
-console.log('Hello world! :)')
+const createRepository = new CreateRepository()
+const obj: CreateObject = {
+    username: 'Pepe'
+}
 
-hola()
+async function create(repository: ICreateRepository): Promise<void> {
+    console.log(await repository.create(obj))
+}
+
+create(createRepository)
+    .then(() => {
+        create(new TransactionDecorator(createRepository)).catch(console.error)
+    })
+    .catch(console.error)
---
+++ src/interfaces/CreateObject.ts
@@ -0,0 +1,3 @@
+export interface CreateObject {
+    [key: string]: any
+}
